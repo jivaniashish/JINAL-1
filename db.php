b99cb25c8ccdbb8290e46b0293121f77e9c5f3b1
jivaniashish <32770296+jivaniashish@users.noreply.github.com>
2020-04-11 19:53:01 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,16 @@
 <?php
 
-$db = new PDO('mysql:host=172.31.22.43;dbname=Jinal_Pareshkumar200442270', 'Jinal_Pareshkumar200442270', 'N4ZACyc65_');
+//$db = new PDO('mysql:host=172.31.22.43;dbname=Jinal_Pareshkumar200442270', 'Jinal_Pareshkumar200442270', 'N4ZACyc65_');
 
+try {
+    $conn =new PDO('mysql:host=172.31.22.43;dbname=Jinal_Pareshkumar200442270', 'Jinal_Pareshkumar200442270', 'N4ZACyc65_');
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+    
 ?>
